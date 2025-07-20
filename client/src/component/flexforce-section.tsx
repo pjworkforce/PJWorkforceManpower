f1683fb8bce564cfa592e3665dafb5b5bd088762
PJ Workforce <prabhakaran@pjworkforce.info>
2025-07-20 15:03:13 +0100

    Update flexforce-section.tsx

--- client/src/component/flexforce-section.tsx
+++ client/src/component/flexforce-section.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button.tsx";
 
 export default function FlexForceSection() {
   const scrollToSection = (sectionId: string) => {
