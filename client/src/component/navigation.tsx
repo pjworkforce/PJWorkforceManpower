ba0305ac6c24a7afd27d4668d89aeb8307946aff
PJ Workforce <prabhakaran@pjworkforce.info>
2025-07-20 15:08:56 +0100

    Update navigation.tsx

--- client/src/component/navigation.tsx
+++ client/src/component/navigation.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button } from "@/components/ui/button.tsx";
+import { Button } from "./ui/button";
 
 export default function Navigation() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
