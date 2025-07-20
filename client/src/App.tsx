67835de38865d0912931e030a10c6db29315439d
PJ Workforce <prabhakaran@pjworkforce.info>
2025-07-20 15:27:32 +0100

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import Home from "./pages/home";
+import Home from "./pages/home-simple";
 import NotFound from "./pages/not-found";
 
 function Router() {
