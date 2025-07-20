2bd4f9b3e3ffb0c7000a1193d9198fd4be6a7267
PJ Workforce <prabhakaran@pjworkforce.info>
2025-07-20 15:02:53 +0100

    Update room-attendant-tiers.tsx

--- client/src/component/room-attendant-tiers.tsx
+++ client/src/component/room-attendant-tiers.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/button.tsx";
 
 export default function RoomAttendantTiers() {
   const scrollToSection = (sectionId: string) => {
